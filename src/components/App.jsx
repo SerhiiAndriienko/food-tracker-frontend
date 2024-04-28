81232ded7166f79605c40ce8141854a861e31c2a
Serhii Andriienko <116962959+SerhiiAndriienko@users.noreply.github.com>
2024-04-28 14:55:01 +0200

    add main page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import DailyGoal from './dailyGoal/DailyGoal';
 import Header from './header/Header';
 import Home from 'pages/home/Home';
 
